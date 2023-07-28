03533dc9ddadefbc6ca8f4ad8a92d3b099e04995
Mat McCann <matmccann@gmail.com>
2023-07-28 17:46:50 -0400

    libLexActivator.so

--- iva/main.cpp
+++ iva/main.cpp
@@ -1,7 +1,9 @@
 #include "Application.h"
 #include "logging.hpp"
 #include "SoftwareLicense.h"
+
 #include <iostream>
+#include <fstream>
 
 using namespace security;
 using namespace core;
@@ -11,26 +13,22 @@ std::string BASE_DIR;
 int main(int argc, char *argv[])
 {
 
-  if (argc < 2) {
-    std::cerr << "Usage    | $ " << argv[0] << " /path/to/.cache\n";
-    std::cerr << "Try this | $ " << argv[0] << " /tmp/.cache\n";
-    return 1;
-  }
-
-  BASE_DIR = argv[1];
+  // check that .cache exists
+  BASE_DIR = ".cache";
+  std::ifstream f(BASE_DIR);
+  if(!f.good())
+    LOG(FATAL) << "Could not find main project directory (" << BASE_DIR << "). Check your path";
 
   SoftwareLicense license;
   bool success = license.start();
   if(!success)
     return EXIT_FAILURE;
 
+  /* Set up logging. Note that MY_LOG_LEVEL is set in CMakeLists.txt */
 #ifndef MY_LOG_LEVEL
 #define MY_LOG_LEVEL 1 // Default log level (INFO)
 #endif
-
   FLAGS_minloglevel = MY_LOG_LEVEL;
-  /* Set up logging. Note that GOOGLE_LOG_LEVEL is set in CMakeLists.txt */
-  std::cout << "FLAGS_minloglevel=" << FLAGS_minloglevel << std::endl;
   core::Logging::init(argv);
   google::InstallFailureSignalHandler();
 
